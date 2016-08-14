5f43d105d394558e4f3cd5c09a3ae302d83841a5
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-08-13 23:03:34 -0700

    get seed to work with several todos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.new( first_name: "user1first", last_name: "user1last", email: "user1@example.com")
+user2 = User.new( first_name: "user2first", last_name: "user2last", email: "user2@example.com")
+user3 = User.new( first_name: "user3first", last_name: "user3last", email: "user3@example.com")
+
+users = [user1, user2, user3]
+
+users.each do |user |
+	user.save
+end
+
+users.each_with_index do | user, user_index | 
+		
+	(1..10).to_a.each do | index |
+		user.todos.create( title: "title#{index}", detail: "detail#{index}", status: "status#{index}")
+	end	
+
+end
